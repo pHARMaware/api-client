cb455e03b8d131d6b8000653d46dbc8fecdecfa8
Matthew Palmer <mspalmer91@gmail.com>
2023-06-13 01:27:27 -0400

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,43 @@
+'use strict';
+
+module.exports = {
+  root: true,
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    sourceType: 'script',
+    ecmaVersion: 2022,
+  },
+  rules: {
+    strict: [2, 'global'],
+  },
+  overrides: [
+    {
+      files: ['**/*.ts'],
+      extends: [
+        'airbnb-base',
+        'airbnb-typescript/base',
+      ],
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+      rules: {
+        '@typescript-eslint/lines-between-class-members': 0,
+        'import/prefer-default-export': 0,
+      },
+    },
+    // {
+    //   files: [
+    //     'jest.setup.ts',
+    //     './tests/**/*.ts',
+    //     '**/__tests__/*.test.ts',
+    //     '**/__mocks__/*.ts',
+    //   ],
+    //   plugins: ['jest'],
+    //   env: {
+    //     'jest/globals': true,
+    //   },
+    // },
+  ],
+};
